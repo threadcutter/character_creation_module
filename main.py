cb0e3a26e255c86840b967966165095a086c1eae
threadcutter <i@paveldemkin.ru>
2023-02-24 01:50:14 +0300

    add working version with classes

--- main.py
+++ main.py
@@ -2,6 +2,65 @@
 
 from random import randint
 
+DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
+
+
+class Character:
+    # Новая константа.
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    RANGE_VALUE_ATTACK = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
+    SPECIAL_BUFF = 15
+    SPECIAL_SKILL = 'Удача'
+
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+
+    def defence(self):
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона')
+
+    def special(self):
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}"')
+
+    # Новый метод базового класса.
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
+
+
+class Warrior(Character):
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
+                             'Сильный, выносливый и отважный')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+    SPECIAL_BUFF = DEFAULT_STAMINA + 25
+    SPECIAL_SKILL = 'Выносливость'
+
+
+class Mage(Character):
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
+                             'Обладает высоким интеллектом')
+    RANGE_VALUE_ATTACK = (5, 10)
+    RANGE_VALUE_DEFENCE = (-2, 2)
+    SPECIAL_BUFF = DEFAULT_ATTACK + 40
+    SPECIAL_SKILL = 'Атака'
+
+
+class Healer(Character):
+    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
+                             'Черпает силы из природы, веры и духов')
+    RANGE_VALUE_ATTACK = (-3, -1)
+    RANGE_VALUE_DEFENCE = (2, 5)
+    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
+    SPECIAL_SKILL = 'Защита'
+
 
 def attack(char_name, char_class):
     """ Расчитывает атаку."""
@@ -88,12 +147,12 @@ def choice_char_class():
 def main():
     """ Вход в игру"""
     if __name__ == '__main__':
-        #run_screensaver()
+        # run_screensaver()
         print('Приветствую тебя, искатель приключений!')
         print('Прежде чем начать игру...')
         char_name = input('...назови себя: ')
         print(f'Здравствуй, {char_name}! '
-            'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+              'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
         print('Ты можешь выбрать один из трёх путей силы:')
         print('Воитель, Маг, Лекарь')
         char_class = choice_char_class()
